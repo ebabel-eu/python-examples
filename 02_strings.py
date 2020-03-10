ec16dd73488a1e8328e92c75e8363a8c573890a3
Thomas Amar <hello@ebabel.eu>
2020-03-10 23:09:24 +0100

    Add strings script.

---
+++ 02_strings.py
@@ -0,0 +1,8 @@
+message = "This is a meta message in double quotes"
+print(message)
+
+message2 = 'This is an actual " character'
+print(message2)
+
+message3 = "Who said \"He's looking at you kid?\" and then left."
+print(message3)
