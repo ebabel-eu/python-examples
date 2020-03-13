a34e5834a3e3c2b4312e16dc8ea12e2d0d239c52
Thomas Amar <hello@ebabel.eu>
2020-03-13 21:09:13 +0100

    Add arithmetics exercises.

---
+++ 04_arithmetic.py
@@ -0,0 +1,22 @@
+hitPoints = 20
+print(hitPoints)
+
+damageFactor = 2
+hitPoints = hitPoints - damageFactor
+print(hitPoints)
+
+poison = 3
+hitPoints = hitPoints / poison
+print(hitPoints)  # Always returns a float.
+
+print(5 / 8)
+print(4 / 1.5)
+print(2/1)
+
+print(4.0 * 5)
+print(4 * 5.1)
+
+print(1+1)
+print(1)
+
+print('Your hit points are ' + str(int(hitPoints)))
